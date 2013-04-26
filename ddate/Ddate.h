a739a8b30aa96d683f62acc9ab927b425d682e5f
Christopher Loessl <cloessl@x-berg.de>
2013-04-26 15:31:45 +0200

    Class Ddate added

---
+++ ddate/Ddate.h
@@ -0,0 +1,13 @@
+//
+//  Ddate.h
+//  ddate
+//
+//  Created by Christopher Loessl on 4/26/13.
+//  Copyright (c) 2013 Christopher Loessl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Ddate : NSObject
+
+@end
---
+++ ddate/Ddate.m
@@ -0,0 +1,13 @@
+//
+//  Ddate.m
+//  ddate
+//
+//  Created by Christopher Loessl on 4/26/13.
+//  Copyright (c) 2013 Christopher Loessl. All rights reserved.
+//
+
+#import "Ddate.h"
+
+@implementation Ddate
+
+@end
