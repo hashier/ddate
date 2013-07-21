6f3cdec817c43a28468192f24170fa2dd778b525
Christopher Loessl <cloessl@x-berg.de>
2013-07-22 01:48:15 +0200

    LaunchImages and IconImages added
    and a script to generate the IconImages

---
+++ ddate/genimages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+svg2png ddate.svg ddate29.png --width 29
+svg2png ddate.svg ddate57.png --width 57
+svg2png ddate.svg ddate58.png --width 58
+svg2png ddate.svg ddate114.png --width 114
+svg2png ddate.svg ddate120.png --width 120
