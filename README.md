59b7d3a509ea8dc1a9393b78e23c36965a6ff1c4
Christopher Loessl <cloessl@x-berg.de>
2013-07-24 17:08:30 +0200

    Documentation added and README updated

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 ddate
 =====
 
-Convert normal boring dates to cool discordian ones
+Convert normal boring dates to cool discordian ones.
+This is implemented as a category so just drop the files into your project and NSDate automatically gets the ability to convert dates to discordian format.
+
+The name derives from the Linux/Unix command line tool named `ddate` that printed the current discordian date.
+
+There is ana iOS project included in the source so you can see how to use it.
+
+
+Doc
+---
+
+You can simply build your own documentation with:
+
+        appledoc --project-name="ddate" --project-company="Christopher Loessl" --company-id="org.loessl" --project-version="1.0" ddate
 
 
 ToDo and ideas
@@ -13,7 +26,9 @@ See issues tracker
 License
 =======
 
-The app and its source code are distributed under MIT License:
+The iOS app is distributed under GPLv3 License.
+
+The category is distributed under MIT License:
 
 Copyright (C) 2013 Christopher Loessl
 
--- ddate/NSDate+ddate.h
+++ ddate/NSDate+ddate.h
@@ -8,27 +8,96 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ Simple category for NSDate that provides discordian dates.
+
+ If you want to build your own string don't forget to check for leap days.
+ The name derives from the command tool ddate which returns a discordian date unter Unix/Linux systems.
+ */
 @interface NSDate (ddate)
 
 #pragma mark - Discordian calendar
-// all in all, checks for leap days and returns everything (incl. holydays)
+
+/**
+ Returns a string that can be directly displayed. It takes care of everything like leap days and discordian holydays
+ 
+ @return A complete ddate string which has taken care of everything.
+ */
 - (NSString *)ddate;
-// if you want to build your own string
-// don't forget to first check if it's a leap day
-- (NSString *)dleapDay; // will return an empty string on non leap days
+
+/**
+ Will return an empty string on non leap days.
+ 
+ @return On leap days it returns "St. Tib's Day, in the YOLD %d" otherwise it returns @"".
+ */
+- (NSString *)dleapDay;
+
+/**
+ @return Returns back the YOLD
+ */
 - (NSUInteger)dyear;
+
+/**
+ @return Returns the current day in the season.
+ */
 - (NSUInteger)dayInSeason;
+
+/**
+ @return Returns apostel (eg Mungday) or holyday (eg. Chaoflux) on holydays and @"" if date is not a holyday.
+ */
 - (NSString *)dholyday;
+
+/**
+ @return Retuns The weekday (eg. Sweetmorn).
+ */
 - (NSString *)dweekday;
+
+/**
+ @return Retuns The weekday (eg. Chaos).
+ */
 - (NSString *)dseason;
-- (NSUInteger)ddayOfYear; // this returns 356 EVEN in leap years! NEEDED for ddate
+
+/**
+ This returns 356 EVEN in leap years! NEEDED for ddate.
+ 
+ @return Returns the day of the year how ddate needs it.
+ */
+- (NSUInteger)ddayOfYear;
 
 #pragma mark - Gregorian calendar
-- (NSUInteger)dayOfYear; // this actually return 366 in leap years!
+
+/**
+ This returns 366 in leap years.
+ 
+ @return Returns day in year.
+ */
+- (NSUInteger)dayOfYear;
+
+/**
+ @return Returns day in month.
+ */
 - (NSInteger)day;
+
+/**
+ @return Returns month in year.
+ */
 - (NSInteger)month;
+
+/**
+ @return returns the year.
+ */
 - (NSInteger)year;
+
+/**
+ @return Returns TRUE in leap years.
+ */
 - (BOOL)isLeapYear;
+
+/**
+ Is today's day a leap day?
+ 
+ @returns Returns TRUE if today's day is a leap day.
+ */
 - (BOOL)isLeapDay;
 
 @end
