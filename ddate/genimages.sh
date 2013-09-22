8998483c84e6992862a44d319bb94682c5b62525
Christopher Loessl <cloessl@x-berg.de>
2013-09-22 20:50:14 +0200

    Prepare for AppStore upload
    Added Screenshots
    Auto inc. auf build number
    Set team

--- ddate/genimages.sh
+++ ddate/genimages.sh
@@ -5,3 +5,4 @@ svg2png ddate.svg ddate57.png --width 57
 svg2png ddate.svg ddate58.png --width 58
 svg2png ddate.svg ddate114.png --width 114
 svg2png ddate.svg ddate120.png --width 120
+svg2png ddate.svg ddate1024.png --width 1024
